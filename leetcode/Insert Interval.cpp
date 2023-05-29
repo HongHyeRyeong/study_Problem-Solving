8478fa7013e571fb7f817f99f2020d37a6d8c37f
hong hye ryeong <h0412l@naver.com>
2023-05-29 15:06:09 +0900

    배열

---
+++ leetcode/Insert Interval.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b) {
+            if (a[0] == b[0])
+                return a[1] < b[1];
+            return a[0] < b[0];
+            });
+
+        vector<vector<int>> answer;
+        answer.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (answer[answer.size() - 1][1] >= intervals[i][0])
+                answer[answer.size() - 1][1] = max(intervals[i][1], answer[answer.size() - 1][1]);
+            else
+                answer.push_back(intervals[i]);
+        }
+
+        return answer;
+    }
+    }
+};
